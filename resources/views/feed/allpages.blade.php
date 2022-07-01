cc6a001679ba6a9959362df2eb7c716c1d5670b9
sans1960 <asanscliment@gmail.com>
2022-07-01 15:19:25 +0200

    feed allpages

--- resources/views/feed/allpages.blade.php
+++ resources/views/feed/allpages.blade.php
@@ -2,7 +2,7 @@
 <rss version="2.0">
     <channel>
         <title><![CDATA[ SoujournPlanet ]]></title>
-        <link><![CDATA[{{  env('APP_URL') }}/feed/general ]]></link>
+        <link><![CDATA[{{  env('APP_URL') }}/feed/allpages ]]></link>
         <description><![CDATA[ Blog ]]></description>
         <language>en</language>
         <pubDate>{{ now()->toDayDateTimeString('Europe/Madrid') }}</pubDate>
@@ -10,10 +10,12 @@
         @foreach($pages as $page)
             <item>
                 <title><![CDATA[{!! $page->name !!}]]></title>
-                  <description> <![CDATA[<img src="{{ asset('public/storage/pages/'.$page->image) }}" width="100" height="50"/>]]> </description>
+                  <description> <![CDATA[<img src="{{ asset('public/storage/pages/'.$page->image) }}" width="100" height="50"/>]]><br/><br/>
+                    {{ Str::limit($page->description, 110, '...') }}
+                </description>
                 <link>{{ env('APP_URL') }}/sites/pages/{{ $page->slug }}</link>
 
-
+                <pubDate>{{ $page->date }}</pubDate>
                 <date>{{ $page->created_at->toRssString() }}</date>
             </item>
         @endforeach
